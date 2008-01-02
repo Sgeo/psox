1df4a1c7d167719a5a2de7df8732be0496f0000a
Sgeo <Sgeo@3f4560be-fcb6-4516-b323-ae570ede3bb7>
2008-01-02 18:06:40 +0000

    Prove to yourself that psoxtypes.py really DOES work well..

--- trunk/impl/psox/types_tester.py
+++ trunk/impl/psox/types_tester.py
@@ -1,7 +1,10 @@
 import psoxtypes as types
 
 print types.VARG(types.STRING).fromtype("\x01\x01a\x00\x00")
+#('\x01a',)
 
 print types.VARG(types.FBYTES(1)).fromtype("\x01b\x01c\x01d\x00")
+#('b', 'c', 'd')
 
 print types.VARG(types.VARG(types.STRING)).fromtype("\x01\x01a\x00\x01b\x00\x01c\x00\x00\x01\x011\x00\x012\x00\x0134\x00\x00\x00")
+#(('a', 'b', 'c'), ('1', '2', '34))
