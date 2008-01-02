3008ecd3c9353956eb900225fc779927a4053bc7
Sgeo <Sgeo@users.noreply.github.com>
2008-01-02 00:16:14 +0000

    Latest information on the looks of domains..

--- ex/example_domain.py
+++ ex/example_domain.py
@@ -1,30 +1,35 @@
 #Note: This is an early example of what a domain might look like. Try not to take it too seriously
 
-from psox.utils import n2ln, s2ls #num to longnum and string to longbytes
-from psox.safety import unsafe
-from psox.domain import Domain, argtypes, funcnum
-from psox.types import STRING, LBYTES, LNUM, FNUM, VARG
+
+from domain import Domain, argtypes, rettypes
+from types import STRING, LBYTES, LNUM, FNUM, VARG
+from psoxglobals import SAFETYLIST
+
+PSOX_VERSION = (0, 0, 0) #First number specifies major version, next two specify minimum minor version, and current minor version
+
+if("noexdomain" not in SAFETYLIST):
+    MY_VERSION = 1 #MY_VERSION MUST be assigned to
+else:
+    MY_VERSION = 0 #Disables the domain
+
 
 class MyDomain(Domain):
-    
-    @funcnum(0x05) #[MANDATORY] This will make this function be bound to 0x05 in this domain
     @argtypes(LNUM, LNUM, FNUM(3)) #[MANDATORY] This specifies what arguments the function will take. Note that the FNUM(3) indicates fixed bytes acting as one number
-    def add(self, num1, num2, num3):
+    @rettypes(LNUM) #[OPTIONAL] Converts the returned Python values into something accepted by PSOX
+    def f05(self, num1, num2, num3): #Note that the name of the function specifies the function number. Hex digits are capitalized
         """Add two longnums, resulting in a longnum"""
-        return n2ln(num1+num2) #The return must be a string..
+        return num1+num2 #The return must be a string..
     
-    
-    @funcnum(0x06)
-    @unsafe #Note that this comes _directly_ after @funcnum
-    @argtypes(FNUM(1), FNUM(1))
-    def unsafe_add(self, num1, num2):
-        return chr(num1+num2)
+    if("no06" not in SAFETYLIST): #How to check for safety stuff
+        @argtypes(FNUM(1), FNUM(1))
+        def f06(self, num1, num2):
+            return chr(num1+num2) #Not bothering to use @rettypes here, returning a sheer string
         
         
-    @funcnum(0x07)
-    @argtypes(VARG(LNUM)) #You can use VARG(sometype) only as the last type in argtypes. The caller, when it reaches that point, will specify the number of arguments it wishes to send. i.e. to add 5 longnums, this would be called with 0x05 0x07 0x05 ln1 ln2 ln3 ln4 ln5
-    def varadd(self, nums*):
-        return n2ln(sum(nums))
+    @argtypes(VARG(LNUM)) #This will cause a tuple to be passed in
+    @rettypes(LNUM)
+    def f07(self, nums):
+        return sum(nums)
         
 
-the_domain = MyDomain() #the_domain is MAGIC
+the_domain = MyDomain #the_domain is MAGIC. Note that we do not create an instance..
