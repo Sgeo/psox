0512d925aa51e411e0f1a8cc4c140ff87c097147
Sgeo <Sgeo@users.noreply.github.com>
2008-02-07 01:30:11 +0000

    Bring fd.py up-to-date with the FD Overhaul

--- impl/psox/fd.py
+++ impl/psox/fd.py
@@ -1,27 +1,19 @@
+import psoxglobals as G
+
 class FdDict(dict):
     def append(self, thing):
         self[sorted(list(set(range(2,256)) - set(self.keys())))[0]] = thing
         
-#The duplication in the below code is historical. There is no difference between out_ and in_, with the notable and critical exception of STDFD, which is why this code is remaining the same
         
 class PSOXFd(object):
-    def out_absseek(self, num):
+    def absseek(self, num):
         raise NotImplementedError
-    def in_absseek(self, num):
+    def relseek(self, num):
         raise NotImplementedError
-    def out_relseek(self, num):
+    def flush(self):
         raise NotImplementedError
-    def in_relseek(self, num):
-        raise NotImplementedError
-    def in_flush(self):
-        raise NotImplementedError
-    def out_flush(self):
-        raise NotImplementedError
-    def in_close(self):
-        raise NotImplementedError
-    def out_close(self):
-        raise NotImplementedError
-        
+    def close(self):
+        raise NotImplementedError        
     def write(self, stuff):
         raise NotImplementedError
     def read(self, bytes):
@@ -29,36 +21,19 @@ class PSOXFd(object):
         
         
 class FdWrapper(PSOXFd):
-    def __init__(self, infile = None, outfile = None):
-        if infile is not None:
-            self.infile = infile
-        if outfile is not None:
-            self.outfile = outfile
-    
-    def out_absseek(self, num):
+    def __init__(self, filelike):
+        self.filelike = filelike    
+    def absseek(self, num):
         if(num>=0):
-            self.outfile.seek(num, 0)
+            self.filelike.seek(num, 0)
         else:
-            self.outfile.seek(num, 2)
-    def in_absseek(self, num):
-        if(num>=0):
-            self.infile.seek(num, 0)
-        else:
-            self.infile.seek(num, 2)
-        
-    def out_relseek(self, num):
-        self.outfile.seek(num, 1)
-    def in_relseek(self, num):
-        self.infile.seek(num, 1)
-        
-    def out_flush(self):
-        self.outfile.flush()
-    def in_flush(self):
-        self.infile.flush()
-    def out_close(self):
-        self.outfile.close()
-    def in_close(self):
-        self.infile.close()
+            self.filelike.seek(num, 2)        
+    def relseek(self, num):
+        self.filelike.seek(num, 1)     
+    def flush(self):
+        self.filelike.flush()
+    def close(self):
+        self.filelike.close()
     
     def write(self, text):
         self.outfile.write(text)
@@ -70,4 +45,12 @@ class FdWrapper(PSOXFd):
       
       
 import sys
-STDFD = FdWrapper(infile = sys.stdin, outfile = sys.stdout)
+STDIN_FD = FdWrapper(sys.stdin)
+STDOUT_FD = FdWrapper(sys.stdout)
+G.FDDICT = FdDict()
+G.FDDICT[2] = STDOUT_FD
+G.FDDICT[3] = STDIN_FD
+G.FDDICT[0] = STDOUT_FD
+G.FDDICT[1] = STDIN_FD
+#Code throughout the project should generally write to G.FDDICT[0] and read from G.FDDICT[1]
+#Changing the current FDs is simply assigning to G.FDDICT[0] and/or G.FDDICT[1]
