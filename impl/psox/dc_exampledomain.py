007484823a80bfb52cee47b2fe621000b2a368cc
Sgeo <Sgeo@users.noreply.github.com>
2008-02-13 21:52:48 +0000

    Forgot to include some stuff

---
+++ impl/psox/dc_exampledomain.py
@@ -0,0 +1,35 @@
+#Note: This is an early example of what a domain might look like. Try not to take it too seriously
+
+
+from domain import Domain, argtypes, rettypes
+from psoxtypes import STRING, LBYTES, LNUM, FNUM, VARG
+from psoxglobals import SAFETYLIST
+
+PSOX_VERSION = (0, 0, 0) #First number specifies major version, next two specify minimum minor version, and current minor version
+
+if("noexdomain" not in SAFETYLIST):
+    MY_VERSION = 1 #MY_VERSION MUST be assigned to
+else:
+    MY_VERSION = 0 #Disables the domain
+
+
+class MyDomain(Domain):
+    @argtypes(LNUM, LNUM, FNUM(3)) #[MANDATORY] This specifies what arguments the function will take. Note that the FNUM(3) indicates fixed bytes acting as one number
+    @rettypes(LNUM) #[OPTIONAL] Converts the returned Python values into something accepted by PSOX
+    def f05(self, num1, num2, num3): #Note that the name of the function specifies the function number. Hex digits are capitalized
+        """Add two longnums, resulting in a longnum"""
+        return num1+num2 #The return must be a string, but @rettypes takes care of that for us
+    
+    if("no06" not in SAFETYLIST): #How to check for safety stuff
+        @argtypes(FNUM(1), FNUM(1))
+        def f06(self, num1, num2):
+            return chr(num1+num2) #Not bothering to use @rettypes here, returning a sheer string
+        
+        
+    @argtypes(VARG(LNUM)) #This will cause a tuple to be passed in
+    @rettypes(LNUM)
+    def f07(self, nums):
+        return sum(nums)
+        
+
+the_domain = MyDomain #the_domain is MAGIC. Note that we do not create an instance..
---
+++ impl/testcustdomain.py
@@ -0,0 +1,42 @@
+from sys import stdin, stdout
+
+def send(text):
+    stdout.write(text)
+    stdout.flush()
+    
+def receive(num):
+    return stdin.read(num)
+    
+
+#PSOX-Init
+send("\x00\x07\x01\n")
+send("\x00\x00\n")
+receive(3)
+
+#Map the domain
+send("\x00\x02\x03") #0x00 0x02 0x03 (map custom domain)
+send("\x03") #0x03 (the domain number we want to use for this domain
+send("exdom\x00") #The PRI plus the mandatory NUL that ends STRINGs
+send("\n") #Mandatory 0x0A after every function call
+receive(1) #Throw away the returned version number
+
+#Use the function 0x06
+send("\x00\x03\x06")
+send("a\x01")
+send("\n")
+print receive(1)
+
+#Use the function 0x05
+send("\x00\x03\x05")
+send("\x01\xFF\x01a\x00")#First longnum
+send("\x02\xFF\x01\x00\x00") #Second longnum
+send("\x01\x02\x03") #FNUM(3)
+send("\n") #Mandatory \n
+print receive(3)[1] #Resulting longnum should be 3 bytes, print 2nd
+
+
+#Use the function 0x07
+send("\x00\x03\x07")
+send("\x01\x01\x0A\x00\x01\x01\x0A\x00\x01\x01\x0A\x00\x01\x01\x0A\x00\x01\x01\x0A\x00\x01\x01\x0A\x00\x01\x01\x05\x00\x00") #VARG(LNUM) representing (10,10,10,10,10,10,5)
+send("\n")
+print receive(3)[1]
